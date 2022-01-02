4b3dca2da4af2802282f385a3a0c8006f8f369ee
Henning Merklinger <henning.merklinger@gmail.com>
2022-01-02 21:20:10 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+from os import environ
+
+
+def main():
+  print("Access Token: {}".format(environ.get("ACCESS_TOKEN", None)))
+  env_file = environ.get('GITHUB_ENV', None)
+  if env_file:
+    with open(env_file, "a") as file:
+      file.write("ACCESS_TOKEN={}\n".format("ThisIsASecret"))
+
+if __name__ == "__main__":
+  main()
