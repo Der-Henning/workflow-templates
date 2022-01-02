2f00b0a6f68d57f2f2f8e17cf1e9e098b5e692d7
Henning Merklinger <henning.merklinger@gmail.com>
2022-01-02 22:12:22 +0100

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ def main():
   old_access_token = environ.get("ACCESS_TOKEN", None)
   print("Access Token from secret: {}".format(old_access_token))
   print("Correct Token: {}".format(old_access_token.startswith('ThisIsASecret')))
-  new_access_token = "ThisIsASecret", random()
+  new_access_token = "ThisIsASecret{}".format(random())
   environ["NEWER_ACCESS_TOKEN"] = new_access_token
   env_file = environ.get('GITHUB_ENV', None)
   if env_file:
