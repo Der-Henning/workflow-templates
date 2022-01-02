99bc1bb48fb41c1af080748dd1dfbb020c7106bd
Henning Merklinger <henning.merklinger@gmail.com>
2022-01-02 22:28:27 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,22 @@
 from os import environ
 from random import random
+from cryptography.fernet import Fernet
 
 
 def main():
-  old_access_token = environ.get("ACCESS_TOKEN", None)
+  cipher_suite = Fernet("mykey")
+  
+  encrypted_old_access_token = environ.get("ACCESS_TOKEN", None)
+  old_access_token = cipher_suite.decrypt(encrypted_old_access_token)
+  
   print("Access Token from secret: {}".format(old_access_token))
   print("Correct Token: {}".format(old_access_token.startswith('ThisIsASecret')))
+ 
   new_access_token = "ThisIsASecret{}".format(random())
-  environ["NEWER_ACCESS_TOKEN"] = new_access_token
+  print("New Token: {}".format(new_access_token))
+  
   env_file = environ.get('GITHUB_ENV', None)
+  
   if env_file:
     with open(env_file, "a") as file:
       file.write("NEW_ACCESS_TOKEN={}\n".format(new_access_token))
