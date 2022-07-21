9eda07ededf6142b5f60b9e058e8d95a852b94fb
Henning Merklinger <henning.merklinger@gmail.com>
2022-07-21 08:45:39 +0200

    clean up

--- encrypt-secret.py
+++ encrypt-secret.py
@@ -3,35 +3,33 @@ from random import random
 
 
 def main():
-  ## Get Github environment file
-  ## Only run this part when GITHUB_ENV is set -> workflow detection
-  env_file = environ.get('GITHUB_ENV', None)
-  if env_file:
-    
-    access_token = environ.get("ACCESS_TOKEN")
+    # Get Github environment file
+    # Only run this part when GITHUB_ENV is set -> workflow detection
+    env_file = environ.get('GITHUB_ENV', None)
+    if env_file:
 
+        # Read access token from environment
+        access_token = environ.get("ACCESS_TOKEN")
 
-    print("Access Token: {}".format(access_token))
-    i=0
+        # Even printing the variable in python will display the masked version
+        print("Access Token: {}".format(access_token))
 
-    while i < len(access_token):
-        print(access_token[i])
-        i += 1
+        ######
+        #
+        # Do stuff here
+        # Use old_access_token to access an API ...
+        # Generate/receive the new access token
+        #
+        #####
+        new_access_token = "ThisIsASecret{}".format(random())
 
+        ## New access token will be printed in plain text!
+        print("New Token: {}".format(new_access_token))
 
-    ######
-    #
-    # Do stuff here
-    # Use old_access_token to access an API ...
-    #
-    #####
-      
-    ## Encrypt new access token from API for next run
-    ## Save encrypted token to GITHUB_ENV
-    new_access_token = "ThisIsASecret{}".format(random())
-    print("New Token: {}".format(new_access_token))
-    with open(env_file, "a") as file:
-      file.write("ACCESS_TOKEN={}\n".format(new_access_token))
+        # Write new access token to environment file
+        with open(env_file, "a") as file:
+            file.write("ACCESS_TOKEN={}\n".format(new_access_token))
+
 
 if __name__ == "__main__":
-  main()
+    main()
